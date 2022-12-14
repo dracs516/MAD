a30bcbd0909b00c452b509cb6b806a5cc7bb5ac2
dracs516 <abhayjha.is20@bmsce.ac.in>
2022-12-14 10:06:55 +0530

    radio button

--- 5_CheckBox/app/src/main/java/com/example/a5_checkbox/MainActivity.java
+++ 5_CheckBox/app/src/main/java/com/example/a5_checkbox/MainActivity.java
@@ -4,54 +4,84 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    CheckBox c1,c2,c3;
+    CheckBox c1, c2, c3;
     TextView t;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        c1=findViewById(R.id.option1);
-        c2=findViewById(R.id.option2);
-        c3=findViewById(R.id.option3);
-        t=findViewById(R.id.menu);
-        
+        c1 = findViewById(R.id.option1);
+        c2 = findViewById(R.id.option2);
+        c3 = findViewById(R.id.option3);
+        t = findViewById(R.id.menu);
+        btn = findViewById(R.id.btn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            int count = 0;
+
+            @Override
+            public void onClick(View view) {
+                count++;
+                if (count % 2 != 0) {
+                    btn.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.empty, 0);
+                } else {
+                    btn.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.full, 0);
+                }
+            }
+        });
+
     }
-    
-    public void checkBox(View v){
+
+    public void checkBox(View v) {
         StringBuilder str = new StringBuilder();
-        str.append("Menu: \n");
+        str.append("Order: \n");
         float total = 0;
-        if(c1.isChecked()){
+        if (c1.isChecked()) {
             str.append("Coffee: 50\n");
-            total+=50;
+            total += 50;
             c1.setButtonDrawable(R.drawable.minus);
-        } else{
+        } else {
             c1.setButtonDrawable(R.drawable.add);
         }
-        if(c2.isChecked()){
+        if (c2.isChecked()) {
             str.append("Tea: 30\n");
-            total+=30;
+            total += 30;
             c2.setButtonDrawable(R.drawable.minus);
-        } else{
+        } else {
             c2.setButtonDrawable(R.drawable.add);
         }
-        if(c3.isChecked()){
+        if (c3.isChecked()) {
             str.append("Churot: 20\n");
-            total+=20;
+            total += 20;
             c3.setButtonDrawable(R.drawable.minus);
-        } else{
+        } else {
             c3.setButtonDrawable(R.drawable.add);
         }
-        str.append("Total: "+total);
+        str.append("Total: " + total);
         t.setText(str);
-        
+
     }
+
+
+//    public void hourGlass(View v){
+//        int count =0;
+//        if(count % 2 == 0){
+//            btn.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.empty,0);
+//            count++;
+//        }else{
+//            btn.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.full,0);
+//            count++;
+//        }
+//    }
 }
